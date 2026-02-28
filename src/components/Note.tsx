334e1caba883f38563f4317b7d369283907b4c96
Dorin <dorin@MBPRO-02.local>
2026-02-28 11:38:16 -0500

    basic movement

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,14 +1,50 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const Note = () => {
   const [noteContent] = useState<string>("Test note");
-  const [xPosition] = useState<number>(500);
-  const [yPosition] = useState<number>(100);
+  const [noteId] = useState<string>("001");
+  const [xPosition, setXposition] = useState<number>(500);
+  const [yPosition, setYPosition] = useState<number>(100);
+
+  const dragRef = useRef<{ noteId: string; x: number; y: number }>(null);
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+
+    dragRef.current = {
+      noteId,
+      x: e.clientX,
+      y: e.clientY,
+    };
+  };
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      e.preventDefault();
+      if (dragRef.current === null) return;
+
+      setXposition(e.clientX);
+      setYPosition(e.clientY);
+    };
+
+    const handleMouseUp = () => {
+      dragRef.current = null;
+    };
+
+    window.addEventListener("mouseup", handleMouseUp);
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mouseup", handleMouseUp);
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
 
   return (
     <div
-      className={` p-2 shadow-lg border-slate-400 rounded-xl bg-yellow-200 w-[100px] h-[100px] transform transition-transform`}
+      className="p-2 shadow-lg border-slate-400 rounded-xl bg-yellow-200 w-[100px] h-[100px] transform transition-transform"
       style={{ transform: `translate(${xPosition}px, ${yPosition}px)` }}
+      onMouseDown={handleMouseDown}
     >
       <div className="text-sm text-black">{noteContent}</div>
     </div>
